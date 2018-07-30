306b707a7fd79ab963a56165ceeaa262c16edddb
srinivas7 <vasu4727@gmail.com>
2018-07-30 21:51:07 +0530

    added cell colors

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -14,9 +14,14 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.IndexedColors;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -46,6 +51,9 @@ public class Demo1Application {
         System.out.println("Enter number of iterations: ");
         Scanner scanner = new Scanner(System.in);
         int iterations = scanner.nextInt();
+        System.out.println("Enter count of the input value you need : ");
+        Scanner scanner1 = new Scanner(System.in);
+        int enteredcount = scanner1.nextInt();
         
         
         try {
@@ -54,6 +62,15 @@ public class Demo1Application {
             BufferedReader bufferedReader = null;
             HSSFWorkbook workbook = new HSSFWorkbook();
             HSSFSheet sheet = workbook.createSheet("FirstSheet");  
+            
+            CellStyle cellStyle1 = workbook.createCellStyle();
+           // cellStyle1.setFillForegroundColor(IndexedColors.BLACK.index);
+            cellStyle1.setFillBackgroundColor(IndexedColors.RED.index);
+            
+            CellStyle cellStyle2 = workbook.createCellStyle();
+            //cellStyle2.setFillForegroundColor(IndexedColors.WHITE.index);
+            cellStyle2.setFillBackgroundColor(IndexedColors.GREEN.index);
+            
 
             HSSFRow rowhead = sheet.createRow((short)rowCount);
             rowhead.createCell(0).setCellValue("phrase");
@@ -61,7 +78,6 @@ public class Demo1Application {
             rowhead.createCell(3).setCellValue("Message");
             rowhead.createCell(2).setCellValue("Status");
             while(currentIteration < iterations){
-            	int rowIterationCount = 0;
             	String status = "";
             	ArrayList<String> msgList = new ArrayList<String>();
             	fileReader =  new FileReader(fileName);
@@ -70,16 +86,13 @@ public class Demo1Application {
             	while((line = bufferedReader.readLine()) != null) {
                     
                     if(line.contains(inputValue)) {
-                    	count++;
-                    	
-                    	if(status == "" || status == "success") {
-                    		if(line.contains("error")) {
+                    	count++; 
+                    		if((line.contains("error")) || count!=enteredcount ) {               
                         		status = "fail";
-                        	}else {
-                        		status = "success";
-                        	}
-                    	}
-                    
+                        	//	System.out.println(count);  
+                    			}
+                    		else 
+                        		status ="success";                  	                   
                     	Pattern pattern = Pattern.compile("<(.*?)>");
                     	Matcher matcher = pattern.matcher(line);
                     	parts = line.split(": "); 
@@ -92,31 +105,55 @@ public class Demo1Application {
                  firstRow.createCell(0).setCellValue(inputValue);
                  firstRow.createCell(1).setCellValue(count);
                  //firstRow.createCell(3).setCellValue(parts[1]); //printing count instead of message as getting many messages
-                 firstRow.createCell(2).setCellValue(status);
+                
+
+         	     //style.setFillPattern(CellStyle.SOLID_FOREGROUND);
+                 
+                 HSSFCellStyle styleGreen = workbook.createCellStyle();
+                 styleGreen.setFillForegroundColor(HSSFColor.LIME.index);
+                 styleGreen.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
+                 styleGreen.setFillForegroundColor(IndexedColors.GREEN.index);
+                 
+                 HSSFCellStyle styleRed = workbook.createCellStyle();
+                 styleRed.setFillForegroundColor(HSSFColor.LIME.index);
+                 styleRed.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
+                 styleRed.setFillForegroundColor(IndexedColors.RED.index);
+                 
+         	             	    
+                 if(status=="sucess")
+                 {
+                	 HSSFCell cell = firstRow.createCell(2); 
+                	 cell.setCellValue(status);
+                	 cell.setCellStyle(styleGreen);
+                 }
+                 else
+                 {
+                	 HSSFCell cell = firstRow.createCell(2);
+                	 cell.setCellValue(status);
+                	 cell.setCellStyle(styleRed);
+                 }                                
                  
                  currentIteration++;
                  enteredId = enteredId.add(BigInteger.ONE);
              	 inputValue = ">"+enteredId;
              	
                  Iterator<String> iterator =  msgList.iterator();
-                 System.out.println(msgList.size());
-                 while(iterator.hasNext()) {
-                	 String msg = iterator.next();
+                 System.out.println("array size"+msgList.size());
+                 
+                 String msg = iterator.next();
+                 firstRow.createCell(3).setCellValue(msg);
+                 
+                 for(int i=1;i<msgList.size();i++) {
+                	 msg = iterator.next();
                 	 System.out.println(msg);
                 	 HSSFRow row = sheet.createRow((short) ++rowCount);
-                  	 
-                	 if(rowIterationCount < msgList.size() - 1)
                   		 row.createCell(3).setCellValue(msg);
-                  	 
-                  	 firstRow.createCell(3).setCellValue(msg);
-                  	 rowIterationCount++;
+                  	
                  }
                  msgList = null;
                  count = 0;
-                 rowIterationCount = 0;
-            }
-               
-                
+            }          
+              
             
             File excelFile = new File("abc1.xls");
             FileOutputStream fileOut = new FileOutputStream(excelFile);
@@ -138,4 +175,9 @@ public class Demo1Application {
         	System.out.println("cant create more than 256 columns or something went wrong"+ ex);
         }
     }
+
+	private static HSSFCell cell(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
